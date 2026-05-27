426f67c7811afcee588ce7577984a765b12a19cd
Taysun Wang <yourgithubemail@example.com>
2026-05-27 09:42:55 +0800

    Updated favicon

--- app/layout.tsx
+++ app/layout.tsx
@@ -4,6 +4,12 @@ import "./globals.css";
 export const metadata: Metadata = {
   title: "WANG CORP.",
   description: "International Industrial Coordination & Infrastructure",
+
+  icons: {
+    icon: "/logo11.png",
+    shortcut: "/logo11.png",
+    apple: "/logo11.png",
+  },
 };
 
 export default function RootLayout({
